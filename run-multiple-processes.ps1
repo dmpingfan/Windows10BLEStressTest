db897ead006b7db74f7f1db0879394ba823b648e
Frank <frank@trainerroad.com>
2019-02-05 11:03:27 -0300

    Observation:  Three processes running 10 threads each with 10 watchers seem to get advertisement data at the same rate.  There are pauses at times.

--- run-multiple-processes.ps1
+++ run-multiple-processes.ps1
@@ -1,4 +1,4 @@
 Start-Process .\Windows10BLEStressTest\bin\Debug\Windows10BLEStressTesst.exe -ArgumentList ("-t","1", "-w", "1")
 Start-Process .\Windows10BLEStressTest\bin\Debug\Windows10BLEStressTesst.exe -ArgumentList ("-t","10", "-w", "10")
-Start-Process .\Windows10BLEStressTest\bin\Debug\Windows10BLEStressTesst.exe -ArgumentList ("-t","100", "-w", "10")
-Start-Process .\Windows10BLEStressTest\bin\Debug\Windows10BLEStressTesst.exe -ArgumentList ("-t","10", "-w", "100")
+Start-Process .\Windows10BLEStressTest\bin\Debug\Windows10BLEStressTesst.exe -ArgumentList ("-t","10", "-w", "10")
+Start-Process .\Windows10BLEStressTest\bin\Debug\Windows10BLEStressTesst.exe -ArgumentList ("-t","10", "-w", "10")

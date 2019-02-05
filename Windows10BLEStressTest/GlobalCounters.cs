729a760b159c51197da23a955f5c215e5b68f111
Frank <frank@trainerroad.com>
2019-02-05 10:23:22 -0300

    initial console app runs BluetoothLEAdvertisementWatcher with global counters

---
+++ Windows10BLEStressTest/GlobalCounters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace Windows10BLEStressTesst
+{
+    public static class GlobalCounters
+    {
+        public static int WatchersCreated;
+        public static int WatchersStopped;
+        public static int AdvertisementsSeen;
+
+        public static void DisplayCounters()
+        {
+            Console.Write($"\rCreated: {WatchersCreated}, Stopped: {WatchersStopped}, Advertisements: {AdvertisementsSeen}  ");
+        }
+
+        public static void IncrementCreated()
+        {
+            Interlocked.Increment(ref WatchersCreated);
+            DisplayCounters();
+        }
+
+        public static void IncrementClosed()
+        {
+            Interlocked.Increment(ref WatchersStopped);
+            DisplayCounters();
+        }
+
+        public static void IncrementAdvertisementsSeen()
+        {
+            Interlocked.Increment(ref AdvertisementsSeen);
+            DisplayCounters();
+        }
+    }
+}
---
+++ Windows10BLEStressTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
+
+namespace Windows10BLEStressTesst
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var watcher = new BluetoothLEAdvertisementWatcher();
+            watcher.SignalStrengthFilter = new BluetoothSignalStrengthFilter()
+                {
+                    SamplingInterval = TimeSpan.Zero
+                };
+
+            GlobalCounters.IncrementCreated();
+
+            watcher.Received += WatcherReceived;
+            watcher.Stopped += WatcherStopped;
+
+            watcher.Start();
+
+            Console.WriteLine("\nPress return to exit:\n");
+
+            Console.ReadLine();
+        }
+
+        private static void WatcherStopped(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementWatcherStoppedEventArgs args)
+        {
+            GlobalCounters.IncrementClosed();
+        }
+
+        private static void WatcherReceived(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementReceivedEventArgs args)
+        {
+            GlobalCounters.IncrementAdvertisementsSeen();
+        }
+    }
+}

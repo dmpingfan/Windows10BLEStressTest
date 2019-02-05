a53089e67bfcbb33be31e43e823636aefba167b6
Frank <frank@trainerroad.com>
2019-02-05 10:28:54 -0300

    Observation:  advertisements aren't received if the creating thread goes away.
    Changed test to launch N threads eaching launching M watchers.  Threads immediately go away, no advertisements if M,N=1

--- Windows10BLEStressTest/Program.cs
+++ Windows10BLEStressTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.Devices.Bluetooth;
 using Windows.Devices.Bluetooth.Advertisement;
@@ -10,13 +11,37 @@ namespace Windows10BLEStressTesst
 {
     public class Program
     {
+        public static int NumberOfThreads = 1;
+        public static int NumberOfWatchers = 1;
+
         static void Main(string[] args)
+        {
+            for (var i = 0; i < NumberOfThreads; i++)
+            {
+                new Thread(() =>
+                {
+                    for (var j = 0; j < NumberOfWatchers; j++)
+                    {
+                        Watcher.Start();
+                    }
+                }).Start();
+            }
+
+            Console.WriteLine("\nPress return to exit:\n");
+
+            Console.ReadLine();
+        }
+    }
+
+    public static class Watcher
+    {
+        public static void Start()
         {
             var watcher = new BluetoothLEAdvertisementWatcher();
             watcher.SignalStrengthFilter = new BluetoothSignalStrengthFilter()
-                {
-                    SamplingInterval = TimeSpan.Zero
-                };
+            {
+                SamplingInterval = TimeSpan.Zero
+            };
 
             GlobalCounters.IncrementCreated();
 
@@ -24,10 +49,6 @@ namespace Windows10BLEStressTesst
             watcher.Stopped += WatcherStopped;
 
             watcher.Start();
-
-            Console.WriteLine("\nPress return to exit:\n");
-
-            Console.ReadLine();
         }
 
         private static void WatcherStopped(BluetoothLEAdvertisementWatcher sender, BluetoothLEAdvertisementWatcherStoppedEventArgs args)
